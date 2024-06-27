16b4dec79bd74c85453bf95660d5908ca6bf7bd2
Yoda <131223775+web3yoda@users.noreply.github.com>
2024-06-27 13:40:50 +0800

    update

--- src/useful-utils/install.sh
+++ src/useful-utils/install.sh
@@ -4,8 +4,8 @@ set -e
 echo "Activating feature 'useful-utils'"
 echo "The provided favorite useful-utils is: ${FAVORITE}"
 
-export OS_TYPE=$(uname -s | sed 's/aarch/arm/' | awk '{print(tolower($0))}')
-export OS_ARCH=$(uname -m | sed 's/x86_/amd/' | awk '{print(tolower($0))}')
+OS_TYPE=$(uname -s | awk '{print(tolower($0))}')
+OS_ARCH=$(uname -m | sed 's/x86_/amd/' | sed 's/aarch/arm/' | awk '{print(tolower($0))}')
 
 get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
