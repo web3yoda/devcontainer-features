5c175a63e0f120d6c66bd7cc3ed921a167a35222
Yoda <131223775+web3yoda@users.noreply.github.com>
2024-06-27 12:00:58 +0800

    support multi platform

--- src/useful-utils/install.sh
+++ src/useful-utils/install.sh
@@ -4,6 +4,9 @@ set -e
 echo "Activating feature 'useful-utils'"
 echo "The provided favorite useful-utils is: ${FAVORITE}"
 
+export OS_TYPE=$(uname -s | awk '{print(tolower($0))}')
+export OS_ARCH=$(uname -m | sed 's/x86_/amd/' | awk '{print(tolower($0))}')
+
 get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
     grep '"tag_name":' |                                            # Get tag line
@@ -21,7 +24,7 @@ fi
 
 # Install yq
 echo "Installing yq ${YQ_VERSION}"
-curl -sSL https://github.com/${YQ_REPO}/releases/download/${YQ_VERSION}/yq_linux_amd64 -o /usr/local/bin/yq
+curl -sSL https://github.com/${YQ_REPO}/releases/download/${YQ_VERSION}/yq_${OS_TYPE}_${OS_ARCH} -o /usr/local/bin/yq
 chmod +x /usr/local/bin/yq
 
 if [ "${GUM_VERSION}" = "latest" ]; then
@@ -29,15 +32,15 @@ if [ "${GUM_VERSION}" = "latest" ]; then
 fi
 # Install gum
 echo "Installing gum ${GUM_VERSION}"
-curl -sSL https://github.com/${GUM_REPO}/releases/download/${GUM_VERSION}/gum_$(echo ${GUM_VERSION} | sed 's/v//')_amd64.deb -o gum_amd64.deb
-dpkg -i gum_amd64.deb && rm gum_amd64.deb
+curl -sSL https://github.com/${GUM_REPO}/releases/download/${GUM_VERSION}/gum_$(echo ${GUM_VERSION} | sed 's/v//')_${OS_ARCH}.deb -o gum_${OS_ARCH}.deb
+dpkg -i gum_${OS_ARCH}.deb && rm gum_${OS_ARCH}.deb
 
 if [ "${GOMPLATE_VERSION}" = "latest" ]; then
     GOMPLATE_VERSION=$(get_latest_release ${GOMPLATE_REPO})
 fi
 # Install gomplate
 echo "Installing gomplate ${GOMPLATE_VERSION}"
-curl -sSL https://github.com/${GOMPLATE_REPO}/releases/download/${GOMPLATE_VERSION}/gomplate_linux-amd64-slim -o /usr/local/bin/gomplate
+curl -sSL https://github.com/${GOMPLATE_REPO}/releases/download/${GOMPLATE_VERSION}/gomplate_${OS_TYPE}-${OS_ARCH}-slim -o /usr/local/bin/gomplate
 chmod +x /usr/local/bin/gomplate
 
 if [ "${SESSION_MANAGER_PLUGIN_VERSION}" = "latest" ]; then
@@ -46,5 +49,5 @@ fi
 
 # install session-manager-plugin
 echo "Installing session-manager-plugin latest"
-curl -sSL "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+curl -sSL "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_${OS_TYPE/amd64/64bit}/session-manager-plugin.deb" -o "session-manager-plugin.deb"
 dpkg -i session-manager-plugin.deb && rm session-manager-plugin.deb
